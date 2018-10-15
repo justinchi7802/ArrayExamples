e7e706fef7390bd2bc3acd09270c8d4450c981af
justinchi7802 <43891875+justinchi7802@users.noreply.github.com>
2018-10-15 11:53:02 -0700

    Completed Task 7 and 8

--- Main.java
+++ Main.java
@@ -157,7 +157,16 @@ class Main
          *          ar[2]=30
          *          ar[3]=4
          */
+        System.out.println("** Task 7 **");
         
+        for (int i=0 ; i<ar1.length ; i++)
+        {
+            if ( ar1[i]%2==1)
+                ar1[i] *=10;
+        }
+        
+        for (int i=0 ; i<ar1.length ; i++)
+            System.out.println(ar1[i]);
          /*
           * Task 8
           *    Create an array called ar2odds
@@ -168,7 +177,18 @@ class Main
           *    ar2[2]=2
           *    ar2[3]=3  ->  ar2odds[1]=3
           */
-         
+        System.out.println("* Task 8 *");
+        int k = 0;
+        int[] ar2odds = new int[ar2.length];
+        for (int i=0; i<ar2.length; i++) {
+            if (i%2 == 1) {
+                ar2odds[k]=ar2[i];
+            k++;
+        }
+        }
+        
+        for (int i=0; i<k; i++)
+            System.out.println(ar2odds[i]);
         /*
          * Task 9. In the array ar2, count how many odd numbers you
          * have.  Then create an ew array called ar4.  Copy just the odd
