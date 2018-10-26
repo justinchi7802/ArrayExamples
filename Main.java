3c4c09beaa879c4e1ef85f76bc3a7192df01cc86
justinchi7802 <43891875+justinchi7802@users.noreply.github.com>
2018-10-26 11:24:25 -0700

    Tasks 10-12

--- Main.java
+++ Main.java
@@ -224,12 +224,44 @@ class Main
          * old   ar4: 1 3 5 7 9
          * new   ar4  9 1 3 5 7
          */
+          System.out.println("** Task 10 ** ");
+         System.out.println("Original ar4");
+         for (int i=0 ; i<ar4.length ; i++)
+             System.out.println(ar4[i]);
+        
+
+         int temp10=ar4[ar4.length-1];
+         
+         for (int i=ar4.length-1 ; i>0 ; i--)
+            ar4[i]=ar4[i-1];
+         
+         ar4[0]=temp10;
+
+         System.out.println("New ar4");
+         for (int i=0 ; i<ar4.length ; i++)
+             System.out.println(ar4[i]);
         
         
         /*
          * Task 11.  Reverse the order of elements in ar2
          */
-        
+        System.out.println("** Task 11 **");
+        for (int i=0 ; i<ar2.length ; i++)
+            System.out.println("ar2["+i+"] = "+ar2[i]);
+            
+        int ar2half = ar2.length/2;
+        System.out.println(" "+ar2half);
+        int task11Temp; 
+        for (int i=0 ; i<ar2half ; i++)
+        {
+            task11Temp = ar2[i];
+            System.out.println(" "+i+":"+(ar2.length-i));
+            ar2[i] = ar2[ar2.length-1-i];
+            ar2[ar2.length-1-i]=task11Temp;
+        }
+               
+        for (int i=0 ; i<ar2.length ; i++)
+            System.out.println("ar2["+i+"] = "+ar2[i]);
         
         /*
          * Task 12: 
@@ -254,6 +286,37 @@ class Main
          *  
          *  Count how many words have more than 5 letters.
          */
+        System.out.println("* Task 12 *");
+        String[] ar5 = new String[16];
+        ar5[0]="Four ";
+        ar5[1]="score";
+        ar5[2]="and";
+        ar5[3]="seven";
+        ar5[4]="years";
+        ar5[5]="ago";
+        ar5[6]="our";
+        ar5[7]="fathers";
+        ar5[8]="brought";
+        ar5[9]="forth";
+        ar5[10]="on";
+        ar5[11]=" this";
+        ar5[12]="continent";
+        ar5[13]="a";
+        ar5[14]="new";
+        ar5[15]="nation";
+        
+        int count5=0;
+ 
+        for (int i=0 ; i<ar5.length ; i++)
+        {
+            // ar5[i] is a String object
+            // ar5[2] is "and"
+            // ar5[2].length() is 3.
+ 
+            if (ar5[i].length()>5)
+            count5++;
+        }
+        
         
         /*
          * Task 13
